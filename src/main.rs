63609da81383dea3b09df56c26993d8ac7eef166
luciano-eth <113019900+finxlfantasy@users.noreply.github.com>
2023-12-04 22:26:53 +0100

    adding question struct

--- src/main.rs
+++ src/main.rs
@@ -16,29 +16,29 @@ pub struct DNSHeader {
     ancount: u16,
     nscount: u16,
     arcount: u16,
+    question: DNSQuestion,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 struct DNSQuestion {
     domain_name: String,
     query_type: u16,
     query_class: u16,
 }
 
+struct DomainBytes(Vec<u8>);
+
+impl From<&str> for DomainBytes {
+    fn from(domain: &str) -> Self {
+        DomainBytes(domain.as_bytes().to_vec())
+    }
+}
+
 impl DNSQuestion {
     fn to_bytes(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
-
-        let labels = self.domain_name.split('.').collect::<Vec<&str>>();
-        for label in labels {
-            let label_bytes = label.as_bytes();
-            bytes.push(label.len() as u8);
-            bytes.extend_from_slice(label_bytes);
-        }
-        bytes.push(0); //End of domain name
-
-        // Convert query type to bytes
+        bytes.extend_from_slice(&self.domain_name.as_bytes());
         bytes.extend_from_slice(&self.query_type.to_be_bytes());
-        // Convert query class to bytes
         bytes.extend_from_slice(&self.query_class.to_be_bytes());
         bytes
     }
@@ -46,6 +46,11 @@ impl DNSQuestion {
 
 impl DNSHeader {
     fn new() -> DNSHeader {
+        let question = DNSQuestion {
+            domain_name: "codecrafters.io".to_string(),
+            query_type: 1,  // A record query
+            query_class: 1, // Internet class
+        };
         DNSHeader {
             id: 1234,
             qr: 1,
@@ -56,10 +61,11 @@ impl DNSHeader {
             ra: 0,
             z: 0,
             rcode: 0,
-            qdcount: 0,
+            qdcount: 1, // Set QDCOUNT to 1 because we have one question
             ancount: 0,
             nscount: 0,
             arcount: 0,
+            question,
         }
     }
 
@@ -72,37 +78,27 @@ impl DNSHeader {
         buf[6..8].copy_from_slice(&self.ancount.to_be_bytes());
         buf[8..10].copy_from_slice(&self.nscount.to_be_bytes());
         buf[10..12].copy_from_slice(&self.arcount.to_be_bytes());
+        let question_bytes = self.question.to_bytes();
+        buf.extend_from_slice(&question_bytes);
         buf
     }
 }
+
 const BUFFER_SIZE: usize = 512;
+
 fn main() {
     println!("Logs from your program will appear here!");
-
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; BUFFER_SIZE];
-
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 let _received_data = String::from_utf8_lossy(&buf[0..size]);
                 println!("Received {} bytes from {}", size, source);
                 println!("Received data: {:?}", &buf[..size]);
-
-                let question = DNSQuestion {
-                    domain_name: "codecrafters.io.".to_string(),
-                    query_type: 1,
-                    query_class: 1,
-                };
-                let question_bytes = question.to_bytes();
-
-                let mut header = DNSHeader::new();
-                header.qdcount += 1;
-                let _header_bytes = header.to_bytes();
-
-                let mut response = header.to_bytes();
-                response.extend(question_bytes);
-
+                let header = DNSHeader::new();
+                println!("Question: {:?}", header.question);
+                let response = header.to_bytes();
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
